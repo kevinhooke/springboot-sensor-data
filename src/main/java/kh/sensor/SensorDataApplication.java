45180da8dd9553ee71f7c16b10d275f4095c9dc7
kevinhooke <kevin.hooke@gmail.com>
2023-11-12 14:44:32 -0800

    initial commmit

---
+++ src/main/java/kh/sensor/SensorDataApplication.java
@@ -0,0 +1,13 @@
+package kh.sensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorDataApplication.class, args);
+	}
+
+}
